9ec1a723d2c6efa34520b6d16d9589b74462d8f4
abusaleh2234 <121687863+abusaleh2234@users.noreply.github.com>
2026-02-10 12:24:11 +0600

    lesson word data load and display

--- script/script.js
+++ script/script.js
@@ -6,11 +6,34 @@ const lessonLoad =() => {
 lessonLoad()
 
 const loaDLevelWord = (id) => {
-    const  url = `https:// openapi.programming-hero.com/api/level/${id}`
+    const  url = `https://openapi.programming-hero.com/api/level/${id}`
     // console.log(url);
     fetch(url)
-    .then(res => res.json())
-    .then(data => console.log(data))
+    .then((res) => res.json())
+    .then(data => displayLevelWord(data.data))
+}
+
+const displayLevelWord = (words) => {
+    console.log(words);
+    const wordContainer = document.getElementById("wordContainer")
+    wordContainer.innerHTML = ""
+     
+// {id: 1, level: 3, word: 'Abundant', meaning: null, pronunciation: 'অবানডান্ট'}
+    words.forEach(word => {
+        const wordCard = document.createElement("div")
+        wordCard.innerHTML = `
+        <div class="p-9 bg-white rounded-lg space-y-5 text-center">
+                    <h3 class="text-2xl font-bold">${word.word}</h3>
+                    <p class="text-xl ">Meaning /Pronounciation</p>
+                    <h3 class="text-xl font-medium">"${word.meaning} / ${word.pronunciation}"</h3>
+                    <div class="flex justify-between">
+                        <span class="p-4 bg-sky-100 rounded-md"><i class="fa-solid fa-circle-info"></i></span> <span class="p-4 bg-sky-100 rounded-md"><i class="fa-solid fa-volume-high"></i></span>
+                    </div>
+                </div>
+        `
+        wordContainer.appendChild(wordCard)
+    });
+    
 }
 
 
